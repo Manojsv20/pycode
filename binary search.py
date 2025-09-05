33c1f9dd9a4879c8cdc7ec47f92bfc435e3732fa
Manojsv20 <manojsv2003@gmail.com>
2025-09-05 15:41:45 +0530

    binary search

--- binary search.py
+++ binary search.py
@@ -1,1 +1,16 @@
-
+def bin (list,key):
+    lb=0
+    ub=len(list)-1
+    while(lb<=ub):
+        mid=(lb+ub)//2
+        if(list[mid]==key):
+            print(f"element {list[mid]} is found in {mid}")
+            return 0
+        elif(key>list[mid]):
+            lb=mid+1
+        else:
+            ub=mid-1
+    print("the key is not found")
+list =[1,2,3,4,5,6,8,9,10]
+key=13
+bin(list,key)
