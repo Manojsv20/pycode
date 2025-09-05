01040e50b1e8ceefb62991ba2c52230addb5f52c
Manojsv20 <manojsv2003@gmail.com>
2025-09-05 15:28:19 +0530

    linear search

--- linear search.py
+++ linear search.py
@@ -1,1 +1,10 @@
-
+def linear(list,key):
+    n=len(list)
+    for i in range(n):
+        if(list[i]==key):
+            print("the value {} is found in the index of {}".format(list[i],i))
+            return 0
+    print("the element is not found")
+list=[7,3,90,57,9]
+key=57
+linear(list,key)
